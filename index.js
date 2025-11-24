9155c9f4b30490884d4ab55335c474eb827be6a5
ali mujib <amujib42503@gmail.com>
2025-11-24 16:45:16 -0500

    add basic functions and html

---
+++ index.js
@@ -0,0 +1,35 @@
+// .reduce for following functions
+
+let initial
+let operator 
+let result
+
+function add(array) {
+    return(a + b);
+}
+
+function subtract(a,b) {
+    return(a - b);
+}
+
+function multiply(a,b) {
+    return(a * b);
+}
+
+function divide(a,b) {
+    return(a / b);
+}
+
+function operate(operator, a, b) {
+    if (operator === '+') {
+        return add(a,b);
+    } else if (operator === '-') {
+        return subtract(a,b);
+    } else if (operator === '*') {
+        return multiply(a,b);
+    } else (operator === '/'); {
+        return divide(a,b);
+    }
+}
+
+console.log(operate('*',7,-8));
